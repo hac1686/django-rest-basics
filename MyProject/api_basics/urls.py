ac02f2e0867f607e57d3611d94ad9045bba04fa7
hac1686 <73030989+hac1686@users.noreply.github.com>
2020-11-29 13:31:38 -0500

    add Class Based API Views in views and urls

--- MyProject/api_basics/urls.py
+++ MyProject/api_basics/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import article_list, article_detail # import all the functions
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetails # import all the functions and classes from views
 
 urlpatterns = [
-    path('article/', article_list),
-    path('detail/<int:pk>', article_detail),
+    #path('article/', article_list), # the old one from def article list function
+    path('article/', ArticleAPIView.as_view()), #new one using Class-based Views instead of one function
+    #path('detail/<int:pk>', article_detail),
+    path('detail/<int:id>', ArticleDetails.as_view()), #using the class view instead of function views
         
 ]
--- MyProject/api_basics/views.py
+++ MyProject/api_basics/views.py
@@ -7,9 +7,57 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+
 # Create your views here.
+class ArticleAPIView(APIView):
+
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True) #many=true is to serialize the query set
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+# to update and delete data - GET, PUT, DELETE
+# Article details class will contain all the functions using APIViews
+class ArticleDetails(APIView):
+
+    def get_object(self,id):
+       try:
+           return Article.objects.get(id=id) #try to return the individual article
+
+       except Article.DoesNotExist:
+           return HttpResponse(status=status.HTTP_404_NOT_FOUND) # if id not found, return error
+
+    def get(self, request, id):
+        article = self.get_object(id) # get the article object by id and assign to article
+        serializer = ArticleSerializer(article)  #assign to serializer
+        return Response(serializer.data) # return the rest response
+
+    def put(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+   
+    def delete(self, request, id):
+        article = self.get_object(id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 # @csrf_exempt  -removing, no longer need this
+# function based views
 @api_view(['GET', 'POST'])
 def article_list(request):
 
